05a1a3ffd8fd3042af1edbce6fcbdc9450609f2d
kareem010101 <163684503+kareem010101@users.noreply.github.com>
2025-02-14 16:37:53 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration :2500,
+    reset : true,
+  })
+
+  sr.reveal('.home',{ delay:300, origin:'left' })
+  sr.reveal('.card',{ delay:300, origin:'right' })
+  sr.reveal('.content',{ delay:300, origin:'left' })
+  sr.reveal('.card-body',{ delay:300, origin:'right' })
+
+  sr.reveal('.footer-top',{ delay:300, origin:'left' })
+  sr.reveal('.input text-center',{ delay:350, origin:'right' })
+  sr.reveal('.text-center',{ delay:350, origin:'top' })
+  sr.reveal('.menu-food-content',{ delay:350, origin:'top' })
+  sr.reveal('.menu-food-text',{ delay:350, origin:'bottom' })
+  sr.reveal('.food-main-content',{ delay:350, origin:'bottom' })
+  sr.reveal('.before',{ delay:350, origin:'top' })
+  sr.reveal('.footer-logo',{ delay:350, origin:'top' })
