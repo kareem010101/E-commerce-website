eca2e6fa76ca2660f74ae257252fcad0e887a304
kareem010101 <163684503+kareem010101@users.noreply.github.com>
2025-08-12 21:54:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const sr = ScrollReveal ({
-    distance : '45px',
-    duration :2500,
-    reset : true,
-  })
+// const sr = ScrollReveal ({
+//     distance : '45px',
+//     duration :2500,
+//     reset : true,
+//   })
 
-  sr.reveal('.home',{ delay:300, origin:'left' })
-  sr.reveal('.card',{ delay:300, origin:'right' })
-  sr.reveal('.content',{ delay:300, origin:'left' })
-  sr.reveal('.card-body',{ delay:300, origin:'right' })
+//   sr.reveal('.home',{ delay:300, origin:'left' })
+//   sr.reveal('.card',{ delay:300, origin:'right' })
+//   sr.reveal('.content',{ delay:300, origin:'left' })
+//   sr.reveal('.card-body',{ delay:300, origin:'right' })
 
-  sr.reveal('.footer-top',{ delay:300, origin:'left' })
-  sr.reveal('.input text-center',{ delay:350, origin:'right' })
-  sr.reveal('.text-center',{ delay:300, origin:'top' })
+//   sr.reveal('.footer-top',{ delay:300, origin:'left' })
+//   sr.reveal('.input text-center',{ delay:350, origin:'right' })
+//   sr.reveal('.text-center',{ delay:300, origin:'top' })
  
+
